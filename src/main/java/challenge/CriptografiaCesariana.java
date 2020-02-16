c33e2cd09e3a5f8794d87bc751bef2b714eff225
Durval Printes <durvalprintes@gmail.com>
2020-02-16 14:00:49 -0300

    Função única para criptografia e descriptografia

--- src/main/java/challenge/CriptografiaCesariana.java
+++ src/main/java/challenge/CriptografiaCesariana.java
@@ -3,55 +3,46 @@ package challenge;
 public class CriptografiaCesariana implements Criptografia {
     final int deslocamento = 3;
 
-    @Override
-    public String criptografar(String texto) {
-        if (texto != "") {
-            String cifrado = "";
+    public String cifrar(String texto, int operacao) {
+        if (!texto.isEmpty() && texto != null) {
+			String cifra = "";
             int codigo;
             for (int i = 0; i < texto.length(); i++) {
-                codigo = (int) texto.toLowerCase().charAt(i);
-                if (codigo >= 97 && codigo <= 122) {
-                    if (codigo <= (122 - deslocamento)) {
-                        cifrado+= (char) (codigo + deslocamento);
-                    } else { 
-                        cifrado+= (char) (codigo - 23);
-                    }
-                } else {
-                    cifrado+=(char) codigo;
+                codigo = (int) texto.charAt(i);
+                if (codigo >= 97 && codigo <= 122) { //[a-z]
+                    codigo+=operacao;                    
+                    if (codigo > 122) {
+						codigo = 97 + (codigo - 122);
+                    } 
+                    if (codigo < 97) {
+						codigo = 122 - (97 - codigo);
+					}
                 }
+                cifra+=(char) codigo;
             }
-            return cifrado;
+            return cifra;
         } else {
-            throw new IllegalArgumentException("Erro ao criptografar texto vazio!");
+            throw new IllegalArgumentException("Texto vazio!");
         }
         
     }
 
     @Override
+    public String criptografar(String texto) {
+		return cifrar(texto.toLowerCase(), deslocamento);
+	}
+
+    @Override
     public String descriptografar(String texto) {
-        if (texto != "") {
-            String decifrado = "";
-            int codigo;
-            for (int i = 0; i < texto.length(); i++) {
-                codigo = (int) texto.toLowerCase().charAt(i);
-                if (codigo >= 97 && codigo <= 122) {
-                    if (codigo >= (97 + deslocamento)) {
-                        decifrado+= (char) (codigo - deslocamento);
-                    } else { 
-                        decifrado+= (char) (codigo + 22);
-                    }
-                } else {
-                    decifrado+=(char) codigo;
-                }
-            }
-            return decifrado;
-        } else {
-            throw new IllegalArgumentException("Erro ao descriptografar Texto vazio!");
-        }
-    }
+        return cifrar(texto.toLowerCase(), -1 * deslocamento);
+    }    
     public static void main(String[] args) {
         CriptografiaCesariana app = new CriptografiaCesariana();
-        // System.out.println("\n"+app.descriptografar(app.criptografar("a ligeira raposa marrom saltou sobre o cachorro cansado")));
-        System.out.println("\n"+app.descriptografar(app.criptografar(null)));
+        String texto = "A ligeira raposa marrom saltou sobre o cachorro cansado";
+        try {
+            System.out.printf("\n%s\n", app.descriptografar(app.criptografar(texto)));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
